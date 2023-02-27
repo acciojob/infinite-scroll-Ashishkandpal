c3153b1a98c4cd247e460377837d2b622c552066
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-02-28 01:23:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here!
-
+const infinity = document.querySelector("#infi-list");
+let counter = 11;
+infinity.addEventListener("scroll", function () {
+  if (infinity.scrollHeight <= infinity.offsetHeight + infinity.scrollTop) {
+    const htmlContent = ` 
+    <li class="${counter}">Item ${counter++}</li>
+    <li class="${counter}">Item ${counter++}</li>
+    `;
+    infinity.insertAdjacentHTML("beforeend", htmlContent);
+  }
+});
